f40931b39c9325f7d9dab2d11f2b5cffdea89fc2
Alejandro Rivas <jarlalejor@>
2019-12-06 09:27:04 -0500

    first commit

---
+++ app/index.js
@@ -0,0 +1,46 @@
+const http = require('http')
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const Tasks = require('./Tasks')
+
+const PORT = process.env.PORT || 5002
+const DB = 'mongodb://localhost/dockerizando'
+
+const app = new express()
+const server = http.createServer(app)
+
+app.set('view engine', 'pug')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors('*'))
+
+app.get('/', async (req, res) => {
+  const tasks = await Tasks.find({})
+  const link = process.env.NODE_ENV !== 'production' ? 'http://localhost:3001' : 'http://a3manager.com'
+
+  res.render('index', { link, tasks })
+})
+
+app.get('/guardar', (req, res) => {
+  res.render('form')
+})
+
+app.post('/register', async (req, res) => {
+  await Tasks.create({ name: req.body.name })
+
+  res.redirect('/')
+})
+
+mongoose.Promise = global.Promise
+mongoose.set('useUnifiedTopology', true)
+
+mongoose.connect(DB, { useNewUrlParser: true })
+.then(() => {
+  server.listen(PORT, () => {
+    console.log('Conectado a MongoDB')
+    console.log(`Sever running in http://localhost:${PORT}`)
+  })
+})
---
+++ landing/index.js
@@ -0,0 +1,20 @@
+const http = require('http')
+const express = require('express')
+
+const PORT = process.env.PORT || 3001
+
+const app = new express()
+const server = http.createServer(app)
+
+app.set('view engine', 'pug');
+
+
+app.get('/', (req, res) => {
+  const link = process.env.NODE_ENV !== 'production' ? 'http://localhost:5002' : 'http://app.a3manager.com'
+
+  res.render('index', { link })
+})
+
+server.listen(PORT, () => {
+  console.log(`Sever running in port ${PORT}`)
+})
