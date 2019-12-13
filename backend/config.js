654f2bd24d3f4ff745058495e869df140b2e395c
Alejandro Rivas <jarlalejor@>
2019-12-13 12:26:22 -0500

    api

--- backend/config.js
+++
@@ -1,7 +0,0 @@
-const yaml = require('js-yaml')
-const fs = require('fs')
-
-let fileContents = fs.readFileSync('./settings.yml', 'utf8')
-let data = yaml.safeLoad(fileContents)
-
-module.exports = data
